ebeeb73eb27fbabe851c4a384222515edb48eed2
CarsonJM596 <124809808+CarsonJM596@users.noreply.github.com>
2025-01-25 13:42:38 -0600

    some functionality

---
+++ tic.js
@@ -0,0 +1,74 @@
+const cell = document.querySelectorAll(".cell");
+const status = document.querySelector("#status");
+const restart = document.querySelector("#restart");
+const winCon = [
+    [0, 1, 2], 
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+] //All pssible win conditions in tic tac toe
+let options = ["", "", "", "", "", "", "", "", "",]
+let currPlayer = "X";
+let running = false;
+
+startGame();
+function startGame(){
+    cell.forEach(cell => cell.addEventListener("click", cellClicked));
+    restart.addEventListener("click", restartGame);
+    status.textContent = `${currPlayer}'s turn`;
+    running = true;
+};
+function cellClicked(){
+    const cellIndex = this.getAttribute("cellIndex");
+
+    if (options[cellIndex] != [""] || !running){
+        return;
+    }
+
+    updateCell(this, cellIndex);
+    checkWin();
+}
+function updateCell(cell, index){
+    options[index] = currPlayer;
+    cell.textContent = currPlayer;
+}
+function changePlayer(){
+    currPlayer = (currPlayer == "X") ? "O" : "X"; //Sets player to O if player was equal to X
+    status.textContent = `${currPlayer}'s turn`;
+}
+function checkWin(){
+    let round1 = false;
+    for(let i = 0;  i < winCon.length; i++){
+        const con = winCon[i];
+        const cellA = options[con[0]];
+        const cellB = options[con[1]];
+        const cellC = options[con[2]];
+
+        if (cellA == "" || cellB == "" || cellC == ""){
+            continue;
+        }
+        if (cellA == cellB && cellB == cellC){
+            roundWon = true;
+            break;
+        }
+    }
+
+    if (roundWon){
+        status.textContent = `${currPlayer} wins!`;
+        running = false;
+    }
+    else if(!options.includes("")){
+        status.textContent = `It's a draw!`;
+        running = false;
+    }
+    else{
+        changePlayer();
+    }
+}
+function restartGame(){
+
+}
